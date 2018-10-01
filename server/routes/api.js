16316ab44bbca7e72eb8b063b3ff0c4a14776493
guilhemheinrich <guilhem.heinrich@gmail.com>
2018-10-01 16:20:40 +0200

    Connection to database done

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const neo4j = require('neo4j-driver').v1;
+
 /* GET api listing. */
-router.get('/', (req, res) => {
-  res.send('api works');
+router.get('/neo', (req, res) => {
+    const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "pic3.14"));
+    const session = driver.session();
+    const resultPromise = session.run(
+        'CREATE (a:Person {name: $name}) RETURN a',
+        {name: 'Zongo'}
+      );
+    res.send('Zongo in da place');
 });
+
+router.get('/', (req, res) => {
+    res.send('api works');
+});
+
 
 module.exports = router;
